23734041283cd00459800d3aed602047776e6a47
Loc Le <lpl212757@gmail.com>
2024-09-14 02:17:49 +0700

    feat - install wlogout

--- install/terminal/app-hyprland-extensions.sh
+++ install/terminal/app-hyprland-extensions.sh
@@ -2,7 +2,7 @@
 yay -Sy --noconfirm --needed nwg-look bibata-cursor-theme-bin
 
 # Install hyprpaper and waybar
-yay -Sy --noconfirm --needed waybar hyprpaper
+yay -Sy --noconfirm --needed waybar hyprpaper wlogout
 
 # Install pavucontrol
 yay -Sy --noconfirm --needed pavucontrol
