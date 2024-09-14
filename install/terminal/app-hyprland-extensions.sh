c23f4c4c9066b65b170fecefe87275d21894fbdd
Loc Le <lpl212757@gmail.com>
2024-09-14 16:46:33 +0700

    feat - install wl-clipboard and cliphist

--- install/terminal/app-hyprland-extensions.sh
+++ install/terminal/app-hyprland-extensions.sh
@@ -2,7 +2,7 @@
 yay -Sy --noconfirm --needed nwg-look bibata-cursor-theme-bin
 
 # Install hyprpaper and waybar
-yay -Sy --noconfirm --needed waybar hyprpaper wlogout
+yay -Sy --noconfirm --needed waybar hyprpaper wlogout wl-clipboard cliphist
 
 # Install pavucontrol
 yay -Sy --noconfirm --needed pavucontrol
