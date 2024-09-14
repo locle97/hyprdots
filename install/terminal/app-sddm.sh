78148030313949ee86a1b7498cb30e7815422e24
Loc Le <lpl212757@gmail.com>
2024-09-15 00:26:56 +0700

    feat - update tokyo night sddm

--- install/terminal/app-sddm.sh
+++ install/terminal/app-sddm.sh
@@ -1,5 +1,5 @@
 # Copy sugar-candy theme to /usr/share/sddm/themes if not exists
-sudo cp ../../sddm/sugar-candy/ /usr/share/sddm/themes/ -r -n
+sudo cp ../../sddm/tokyo-night-sddm /usr/share/sddm/themes/ -r -rf
 
 # Install qt5
 yay -Sy --needed --noconfirm sddm qt5-graphicaleffects qt5-quickcontrols2 qt5-svg
@@ -7,4 +7,4 @@ yay -Sy --needed --noconfirm sddm qt5-graphicaleffects qt5-quickcontrols2 qt5-sv
 # Set the sddm theme to /etc/sddm.conf
 # [Theme]
 # Current=sugar-candy
-echo -e "[Theme]\nCurrent=sugar-candy" | sudo tee /etc/sddm.conf
+echo -e "[Theme]\nCurrent=tokyo-night-sddm" | sudo tee /etc/sddm.conf
