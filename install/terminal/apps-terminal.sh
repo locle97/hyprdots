438e20a4232ba564a4480ce3cd985fd9c3e2d820
Loc Le <lpl212757@gmail.com>
2024-09-14 16:31:54 +0700

    feat - update font

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,4 +1,21 @@
 yay -Sy --needed --noconfirm fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr bash-completion oh-my-posh neofetch
 
 # Install nerd font
-yay -Sy --needed --noconfirm ttf-sourcecodepro-nerd
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+unzip CascadiaMono.zip -d CascadiaFont
+cp CascadiaFont/*.ttf ~/.local/share/fonts
+rm -rf CascadiaMono.zip CascadiaFont
+
+wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
+unzip iafonts.zip -d iaFonts
+cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
+rm -rf iafonts.zip iaFonts
+
+fc-cache
+cd -
+
+yay -Sy --needed --noconfirm ttf-sourcecodepro-nerd ttf-freefont ttf-arphic-uming tt
+f-baekmuk
