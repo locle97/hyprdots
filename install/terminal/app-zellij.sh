8973738dc32c3575a8805192e33a126afa57c007
Loc Le <lpl212757@gmail.com>
2024-09-18 18:44:14 +0700

    feat - add script for install zellij

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,1 +1,6 @@
-yay -Sy --needed --noconfirm zellij
+cd /tmp
+wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+tar -xf zellij.tar.gz zellij
+sudo install zellij /usr/local/bin
+rm zellij.tar.gz zellij
+cd -
