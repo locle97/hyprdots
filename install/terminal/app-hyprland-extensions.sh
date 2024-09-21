ce2f4fb5182ded41248d73508bd6e9d89adbfe51
Loc Le <lpl212757@gmail.com>
2024-09-21 14:43:27 +0700

    feat - install imv for preview image and hypridle

--- install/terminal/app-hyprland-extensions.sh
+++ install/terminal/app-hyprland-extensions.sh
@@ -2,7 +2,7 @@
 yay -Sy --noconfirm --needed nwg-look bibata-cursor-theme-bin
 
 # Install hyprpaper and waybar
-yay -Sy --noconfirm --needed wlsunset waybar hyprpaper wlogout wl-clipboard cliphist hyprlock
+yay -Sy --noconfirm --needed wlsunset waybar hyprpaper wlogout wl-clipboard cliphist hyprlock hypridle
 
 # Install pavucontrol
 yay -Sy --noconfirm --needed pavucontrol
