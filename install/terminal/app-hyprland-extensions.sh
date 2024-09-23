6c9161fc9c0266f837e61f9f144d5adbbd097323
Loc Le <lpl212757@gmail.com>
2024-09-24 04:17:50 +0700

    feat - update themes for rofi

--- install/terminal/app-hyprland-extensions.sh
+++ install/terminal/app-hyprland-extensions.sh
@@ -2,7 +2,14 @@
 yay -Sy --noconfirm --needed nwg-look bibata-cursor-theme-bin
 
 # Install hyprpaper and waybar
-yay -Sy --noconfirm --needed wlsunset waybar hyprpaper wlogout wl-clipboard cliphist hyprlock hypridle
+yay -Sy --noconfirm --needed wlsunset waybar hyprpaper wlogout wl-clipboard cliphist hyprlock hypridle rofi
 
 # Install pavucontrol
 yay -Sy --noconfirm --needed pavucontrol
+
+# Instlal rofi themes
+git clone https://github.com/lr-tech/rofi-themes-collection.git
+cd rofi-themes-collection
+
+mkdir -p ~/.local/share/rofi/themes/
+cp themes/* ~/.local/share/rofi/themes/
