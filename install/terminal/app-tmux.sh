1609abbfe95d9a43562ece36e4617121962e2c55
Loc Le <lpl212757@gmail.com>
2024-10-13 14:30:55 +0700

    feat - install tms

--- install/terminal/app-tmux.sh
+++ install/terminal/app-tmux.sh
@@ -2,3 +2,6 @@ yay -Sy --needed --noconfirm tmux
 
 sudo rm -r ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install tms
+cargo install tmux-sessionizer
